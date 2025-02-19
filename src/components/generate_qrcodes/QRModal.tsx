9013dd2f706152125d3839194e094bf101bf30e0
lcfarias1501 <lcfarias1501@gmail.com>
2025-02-19 11:14:36 +0000

    qr modal updated

--- src/components/generate_qrcodes/QRModal.tsx
+++ src/components/generate_qrcodes/QRModal.tsx
@@ -1,40 +1,56 @@
-import React, { useEffect } from "react";
-import { LuX } from "react-icons/lu";
+"use client"
+
+import Image from "next/image"
+import React, { useEffect, useState } from "react"
+import { LuX, LuExternalLink, LuCopy, LuCheck } from "react-icons/lu"
 
 interface Props {
-    image: string;
-    title: string;
-    onClose: () => void;
-    isOpen: boolean;
+    image: string
+    title: string
+    onClose: () => void
+    isOpen: boolean
 }
 
 export default function QRModal({ image, onClose, title, isOpen }: Props) {
+
+    const [isLinkCopied, setIsLinkCopied] = useState<boolean>(false)
+
     // Handle escape key press
     useEffect(() => {
         const handleEscape = (e: KeyboardEvent) => {
             if (e.key === "Escape") {
-                onClose();
+                closeModal()
             }
-        };
-
-        if (isOpen) {
-            window.addEventListener("keydown", handleEscape);
-        } else {
-            window.removeEventListener("keydown", handleEscape);
         }
 
-        return () => window.removeEventListener("keydown", handleEscape);
-    }, [isOpen, onClose]);
+        if (isOpen) {
+            window.addEventListener("keydown", handleEscape)
+        } else {
+            window.removeEventListener("keydown", handleEscape)
+        }
+
+        return () => window.removeEventListener("keydown", handleEscape)
+    }, [isOpen, onClose])
 
     // Handle backdrop click
     const handleBackdropClick = (e: React.MouseEvent) => {
         if (e.target === e.currentTarget) {
-            onClose();
+            closeModal()
         }
-    };
+    }
+
+    function copyLink() {
+        navigator.clipboard.writeText(title)
+        setIsLinkCopied(true)
+    }
+
+    function closeModal() {
+        onClose()
+        setIsLinkCopied(false)
+    }
 
     return (
-         <div 
+        <div
             className={`fixed inset-0 bg-black/50 flex items-center justify-center z-50 m-0 p-0 ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}
             onClick={handleBackdropClick}
             style={{
@@ -47,19 +63,60 @@ export default function QRModal({ image, onClose, title, isOpen }: Props) {
             }}
         >
             <div
-                className={`bg-white p-6 rounded-lg shadow-lg max-w-sm w-full transition-transform duration-300 ${isOpen ? "scale-100" : "scale-95"
+                className={`bg-white rounded-2xl overflow-hidden shadow-lg max-w-sm w-full transition-transform duration-300 ${isOpen ? "scale-100" : "scale-95"
                     }`}
             >
-                <button onClick={onClose} className="absolute top-4 right-4 text-gray-500 hover:text-gray-800">
-                    <LuX size={24} />
-                </button>
+                {/* Title Banner */}
+                <div className="bg-gradient-to-r from-[var(--main)] to-blue-300 p-6">
 
-                <h1 className="text-xl font-semibold text-center">{title}</h1>
-
-                <div className="flex justify-center mt-4">
-                    <img src={image} alt={title} className="max-w-[200px] rounded-md shadow" />
+                    <div className="flex justify-between items-center">
+                        <span className="text-white text-xl font-semibold">QR created!</span>
+                        <button onClick={closeModal} className="text-white hover:text-gray-800">
+                            <LuX size={24} />
+                        </button>
+                    </div>
                 </div>
+
+                {/* Content */}
+                <div className="p-6">
+
+                    {/* QR CODE */}
+                    <div className="flex justify-center my-4">
+                        <Image
+                            src={image}
+                            alt={title}
+                            width={200}
+                            height={100}
+                            priority
+
+                        />
+                    </div>
+
+                </div>
+
+                {/* Action Buttons */}
+                <div className="flex items-center justify-center space-x-4 px-6 pb-6">
+
+                    <a href={title} target="_blank">
+                        <button className="bg-[var(--main)] hover:bg-[var(--main-hover)] rounded-md shadow-sm flex space-x-2 items-center px-6 py-2 text-white">
+                            <LuExternalLink />
+                            <span>Open URL</span>
+                        </button>
+                    </a>
+
+
+                    <button
+                        onClick={copyLink}
+                        className={`${isLinkCopied ? "bg-green-300" : "bg-[var(--main)]"} ${!isLinkCopied && "hover:bg-[var(--main-hover)]"} rounded-md shadow-sm flex space-x-2 items-center px-6 py-2 text-white`}
+                    >
+                        {isLinkCopied ? <LuCheck /> : <LuCopy />}
+                        <span>{isLinkCopied ? "URL Copied" : "Copy URL"}</span>
+                    </button>
+
+
+                </div>
+
             </div>
         </div>
-    );
+    )
 }
